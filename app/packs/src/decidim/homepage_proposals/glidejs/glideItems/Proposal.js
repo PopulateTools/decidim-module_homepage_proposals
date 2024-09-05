c24cd873efcc77866f19ff1b7019d854172a1cbb
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-09-05 12:08:37 +0200

    Change design of proposals cards

--- app/packs/src/decidim/homepage_proposals/glidejs/glideItems/Proposal.js
+++ app/packs/src/decidim/homepage_proposals/glidejs/glideItems/Proposal.js
@@ -20,29 +20,21 @@ export default class Proposal extends GlideItem {
     }
 
     render() {
-        return `<div class="column glide__slide">
-<div class="card card--proposal card--stack">
-    <a href="${this.url}">
-      <div class="proposal-glance card--header">
-      <img src="${this.image}" class="proposal-glance__img" alt="slider_img">
-</div>
-    </a>
-    <div class="card--process__small text-center padding-1">
-        <span class="${this.color} card__text--status status_slider"> ${this.stateI18n} </span>
-        <a href="${this.url}"><h3 class="proposal-glance card__title">${this.title}</h3></a>
-        
-        <div class="card__text--paragraph padding-top-1">
-            ${this.getTagsTemplate()}
-            <p>${this.body}</p>
+        return `<a href="${this.url}" class="card__grid glide__slide">
+      <div class="card__grid-img">
+        <img src="${this.image}" class="card__grid-img" alt="slider_img">
+      </div>
+      <div class="card__grid-text">
+        <div class="card__list-metadata">
+          <span class="label ${this.color}"> ${this.stateI18n} </span>
         </div>
-        <a href="${this.url}">
-            <div class="card__button align-bottom padding-top-1">
-                <span class="button small button--secondary">Visit</span>
-            </div>
-        </a>
-    </div>
-  </div>
-</div>`
+        <h3 class="h4 text-secondary">${this.title}</h3>
+
+          ${this.getTagsTemplate()}
+        <p>${this.body}</p>
+
+      </div>
+    </a>`
     }
 
 }
