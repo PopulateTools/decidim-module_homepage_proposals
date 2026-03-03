43da20940bc581a8022e71d56ee70fa580ba83b4
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2026-03-03 17:32:24 +0100

    Update version and decidim dependency

--- lib/decidim/homepage_proposals/version.rb
+++ lib/decidim/homepage_proposals/version.rb
@@ -3,11 +3,11 @@
 module Decidim
   module HomepageProposals
     def self.version
-      "3.1.0"
+      "3.2.0"
     end
 
     def self.decidim_version
-      "0.29"
+      "0.30"
     end
   end
 end
